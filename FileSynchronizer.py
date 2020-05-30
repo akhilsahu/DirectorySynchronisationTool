3c513626ba8ff2581db02a747ea3916acb3fa3e8
unknown <akhilsahu94@gmail.com>
2020-05-31 01:53:32 +0530

    initial commit

---
+++ FileSynchronizer.py
@@ -0,0 +1,124 @@
+import json
+import os
+import shutil
+import threading
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+import re
+
+from PySide2.QtCore import QObject, Signal, Slot, QUrl
+from collections import defaultdict
+
+
+class FileSynchronizer(QObject):
+    copy_status = Signal(str)
+    progress_bar = Signal(int)
+    json_file_link = Signal(str)
+    def __init__(self, src: 'list', dst: 'str'):
+        QObject.__init__(self)
+        self.input_directory = src
+        self.destination = dst + "/"
+        self.json_dict = defaultdict(
+            lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list)))))
+        self.copy_flag = True
+        self.source_size = 0
+        self.calculateSourceSize()
+
+    def sync(self):
+        print("Reading source directories" + self.destination)
+        if not self.check_exist(self.destination):
+            self.copy_status.emit(
+                "<span style=\" font-size:8pt; font-weight:600; color:red;\" >" + "Invalid Destination Folder. Please check and verify" + "</span>")
+            return
+
+        for dir in self.input_directory:
+            if self.check_exist(dir) and self.copy_flag:
+                files = [f for f in listdir(dir)]
+                print(files)
+                self.process_files(dir, files)
+            else:
+                self.copy_status.emit("Skipping source path " + dir + " Does not Exist")
+        self.generate_json_file()
+
+    def process_files(self, base_path, files):
+
+        for f in files:
+            self.copy_status.emit("Processing in " + base_path + " file : " + f)
+
+            if self.validate_file(f):
+                split__ = f.split("_")
+                yyyymmddhhmm = self.date_from_file(base_path + "/" + f)
+                split_dot = split__[2].split(".")
+
+                destination_base_path = self.destination + split__[0] + "/"
+
+                destination_path = destination_base_path + yyyymmddhhmm + "/" + split__[0] + "_" + split__[
+                    1] + "/" + split_dot[0] + "/" + split_dot[2] + "/" + f
+                source_path = base_path + "/" + f
+
+                self.copy_status.emit("<span style=\" font-size:8pt; font-weight:600; color:blue;\" >"+"Copying " + base_path + " file : " + f + "  to " + destination_path+"</span>")
+
+                self.copy_files(source_path, destination_path)
+                self.progress()
+                self.json_dict[split__[0]][yyyymmddhhmm][split__[0] + "_" + split__[
+                    1]][split_dot[0]][split_dot[2]].append(f)
+            else:
+                self.copy_status.emit("<span style=\" font-size:8pt; font-weight:600; color:red;\" >"+"invalid Name Format. Skipping in " + base_path + " file : " + f+"</span>")
+    def generate_json_file(self):
+        with open('result.json', 'w') as fp:
+            json.dump(self.json_dict, fp)
+            url = bytearray(QUrl.fromLocalFile(
+                    self.destination + "result.json").toEncoded()).decode()
+            text = "<a href={}>{}</a>".format(url,url)
+            self.json_file_link.emit("JSON MANIFEST FILE: "+ text)
+
+
+    def calculateSourceSize(self):
+        for i in self.input_directory:
+            self.source_size += (sum(
+                os.path.getsize(os.path.join(dirpath, filename)) for dirpath, dirnames, filenames in os.walk(i) for
+                filename in filenames))
+
+    def progress(self):
+
+        src_size = self.source_size
+        dst_size = (sum(
+            os.path.getsize(os.path.join(dirpath, filename)) for dirpath, dirnames, filenames in
+            os.walk(self.destination) for
+            filename in filenames))
+        progress_percent = (dst_size / src_size) * 100
+
+        self.copy_status.emit(str(round(progress_percent)) + "% complete")
+        self.progress_bar.emit(round(progress_percent))
+
+    @staticmethod
+    def copy_files(source_path, destination_path):
+        os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+        shutil.copy(source_path, destination_path)
+
+    @staticmethod
+    def check_exist(path):
+        return os.path.exists(path)
+
+    @staticmethod
+    def date_from_file(file_name):
+        try:
+            mtime = os.path.getmtime(file_name)
+        except OSError:
+            mtime = 0
+        return  datetime.fromtimestamp(mtime).strftime('%Y%m%d%H%M')
+
+    @staticmethod
+    def validate_file(f):
+        # "PROJECTNAME_SHOTNAME_TASKNAME.FRAMENUMBER.FILETYPE
+        pattern = "[a-zA-Z]\_[a-zA-Z]+\_[a-zA-Z]+\.[0-9]{4}\.[a-zA-Z]+"
+        return re.search(pattern, f)
+
+    def clean_destination(self):
+        shutil.rmtree(self.destination)
+        print("Clean up directories")
+
+#
+# fds = FileSynchronizer(["E:/test/source1"], "E:/test/destination")
+# fds.sync()
---
+++ main.py
@@ -0,0 +1,131 @@
+import time
+
+from PySide2.QtCore import Qt, Slot
+
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, \
+    QGridLayout, QFileDialog, QTextEdit, QLineEdit, QSpacerItem, QSizePolicy, QProgressBar, \
+    QLabel, QListView, QAbstractItemView, QTreeView
+import sys
+from FileSynchronizer import FileSynchronizer
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Directory Sync Tool")
+        self.setGeometry(200, 200, 700, 700)
+        self.sourceFilePath, self.sourceBtn = self.createSourceSelect("source")
+        self.destinationFilePath, self.destinationBtn = self.createSourceSelect("destination")
+        self.executeButton = QPushButton("Execute")
+        self.cancelButton = QPushButton("Cancel")
+
+        self.myQLabel = QLabel()
+        self.progressbar = QProgressBar()
+        self.progressbar.setMinimum(0)
+        self.progressbar.setMaximum(100)
+        self.textEdit = QTextEdit()
+        self.textEdit.setReadOnly(True)
+        self.createGridLayout()
+        self.source = []
+        self.destination = ""
+        self.executeButton.clicked.connect(self.execute)
+        self.cancelButton.clicked.connect(self.cancel_copy)
+
+    def createGridLayout(self):
+        gridLayout = QGridLayout()
+        gridLayout.addWidget(self.sourceFilePath, 0, 0, 1, 1)
+        gridLayout.addWidget(self.sourceBtn, 0, 1, 1, 1)
+        gridLayout.addWidget(self.destinationFilePath, 1, 0, 1, 1)
+        gridLayout.addWidget(self.destinationBtn, 1, 1, 1, 1)
+        gridLayout.addWidget(self.executeButton, 2, 0, 1, 1)
+        gridLayout.addWidget(self.cancelButton, 2, 1, 1, 1)
+
+        gridLayout.addWidget(self.textEdit, 5, 0, 7, 2)
+        gridLayout.addWidget(self.progressbar, 14, 0, 1, 2)
+        gridLayout.addWidget(self.myQLabel, 16, 0)
+        vspacer = QSpacerItem(
+            QSizePolicy.Minimum, QSizePolicy.Expanding)
+        gridLayout.addItem(vspacer, 0, 0, Qt.AlignTop)
+
+        self.setLayout(gridLayout)
+
+    def createSourceSelect(self, init_text):
+        filePath = QLineEdit()
+        filePath.setPlaceholderText("Choose " + init_text + " folder")
+        filePath.setDisabled(True)
+        btn = QPushButton("Browse", self)
+        if init_text == "source":
+            btn.clicked.connect(lambda: self.getMultipleSelected(init_text))
+        else:
+            btn.clicked.connect(lambda: self.getSeletedFile(init_text))
+        return filePath, btn
+
+    def execute(self):
+        if len(self.source) > 0 and len(self.destination) > 0:
+            self.executeButton.setEnabled(False)
+            self.fds = FileSynchronizer(self.source, self.destination)
+            self.fds.copy_status.connect(self.copy_status)
+            self.fds.progress_bar.connect(self.progress_bar)
+            self.fds.json_file_link.connect(self.json_file_link)
+            self.fds.sync()
+            self.executeButton.setEnabled(True)
+            self.cancelButton.setEnabled(True)
+
+    @Slot(str)
+    def json_file_link(self, data):
+        self.myQLabel.setText(data)
+        self.myQLabel.setOpenExternalLinks(True)
+
+    @Slot(int)
+    def progress_bar(self, pr):
+        print(pr)
+        self.progressbar.setValue(pr)
+
+    @Slot()
+    def copy_status(self, status):
+        self.textEdit.append(status)
+
+        print(status)
+
+    def cancel_copy(self):
+        self.fds.copy_flag = False
+        userInfo = QMessageBox.question(self, "Confirmation",
+                                        "Clean up destination directory? Warning: Entire directory may be deleted in this process",
+                                        QMessageBox.Yes | QMessageBox.No)
+
+        if userInfo == QMessageBox.Yes:
+            self.fds.clean_destination()
+
+        elif userInfo == QMessageBox.No:
+            pass
+
+    def getSeletedFile(self, init_text):
+        path = QFileDialog.getExistingDirectory()
+        self.destination = path
+        eval("self." + init_text + "FilePath").setText(path)
+
+    def getMultipleSelected(self, init_text):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.DirectoryOnly)
+        file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+        file_view = file_dialog.findChild(QListView, 'listView')
+
+        # to make it possible to select multiple directories:
+        if file_view:
+            file_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        f_tree_view = file_dialog.findChild(QTreeView)
+        if f_tree_view:
+            f_tree_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        if file_dialog.exec():
+            self.source = file_dialog.selectedFiles()
+            eval("self." + init_text + "FilePath").setText(",".join(self.source))
+            print(self.source)
+
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+app.exec_()
+sys.exit(0)
